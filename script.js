4fdce8edad3cd8b3e574f25d444aa411705521fe
MD GAFRUJAMA ANSARI <156898012+md-Gafrujama@users.noreply.github.com>
2024-03-27 12:14:22 +0530

    comit changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer . . ." , "Programmer . . ." , "Web Developer . . ."],
+    strings: ["Frontend Developer . . ." , "Programmer . . ." , "MERN Stack Developer . . ."],
     typeSpeed: 70,
     backSpeed: 50,
     backDelay: 1000,
     loop: true
-})
+});
